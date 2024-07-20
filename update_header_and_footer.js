ddbbda565a2ce93b7855bf780fe720c5f88bd56c
Marc Pascal Hollinger <marcpascal.hollinger@uzh.ch>
2024-07-20 20:09:09 +0200

    duplicate from index instead of files

--- update_header_and_footer.js
+++ update_header_and_footer.js
@@ -4,11 +4,10 @@ import { win32 as path } from "node:path";
 
 const html_dir = "./html";
 
-const index_content = fs.readFileSync("./index.html", "utf-8");
-const index_root = parse(index_content);
+const index_data = fs.readFileSync("./index.html", "utf-8");
+const index_root = parse(index_data);
 const header_content = index_root.querySelector("header").innerHTML;
 const footer_content = index_root.querySelector("footer").innerHTML;
-
 
 fs.readdir(html_dir, (err, files) => {
   if (err) {
@@ -17,9 +16,21 @@ fs.readdir(html_dir, (err, files) => {
   }
   files.forEach((file) => {
     const filepath = path.join(html_dir, file);
-    exchange_header_and_footer(filepath, header_content, footer_content);
+    duplicate_to_file(filepath, header_content, footer_content);
   });
 });
+
+function duplicate_to_file(filepath, source_header, source_footer) {
+  const target_data = fs.readFileSync(filepath, "utf-8");
+  const target_root = parse(target_data);
+  target_root.querySelector("header").set_content(source_header);
+  target_root.querySelector("footer").set_content(source_footer);
+  fs.writeFile(filepath, target_root.toString(), (err) => {
+    if (err) {
+      console.log("error writing file");
+    }
+  });
+}
 
 function exchange_header_and_footer(filepath, header_content, footer_content) {
   fs.readFile(filepath, "utf-8", (err, data) => {
