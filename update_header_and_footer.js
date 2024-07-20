6e0abe08f8c3da48cadd466838ea81903868ef3b
Marc Pascal Hollinger <marcpascal.hollinger@uzh.ch>
2024-07-20 18:31:38 +0200

    footer to js

--- update_header_and_footer.js
+++ update_header_and_footer.js
@@ -2,12 +2,12 @@ import { parse } from "node-html-parser";
 import fs from "node:fs";
 import { win32 as path } from "node:path";
 
-const root = parse('<ul id="list"><li>Hello World</li></ul>');
 const html_dir = "./html";
 
 const header_content = fs.readFileSync("./header.html", "utf-8");
+const footer_content = fs.readFileSync("./footer.html", "utf-8");
 
-exchange_header("./index.html", header_content);
+exchange_header_and_footer("./index.html", header_content, footer_content);
 
 fs.readdir(html_dir, (err, files) => {
   if (err) {
@@ -16,11 +16,11 @@ fs.readdir(html_dir, (err, files) => {
   }
   files.forEach((file) => {
     const filepath = path.join(html_dir, file);
-    exchange_header(filepath, header_content);
+    exchange_header_and_footer(filepath, header_content, footer_content);
   });
 });
 
-function exchange_header(filepath, content) {
+function exchange_header_and_footer(filepath, header_content, footer_content) {
   fs.readFile(filepath, "utf-8", (err, data) => {
     if (err) {
       console.error(err);
@@ -28,7 +28,9 @@ function exchange_header(filepath, content) {
     }
     const root = parse(data);
     const header = root.querySelector("header");
-    header.set_content(content);
+    const footer = root.querySelector("footer");
+    header.set_content(header_content);
+    footer.set_content(footer_content);
     fs.writeFile(filepath, root.toString(), (err) => {
       if (err) {
         console.log("error writing file");
